c37ccbf0cead17175ea048ecf92f8c72af0e6bf4
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-07-27 16:09:47 -0400

    Repair Deleted Project
    Repair Hangman code into GitHub

--- main.py
+++ main.py
@@ -1,50 +1,70 @@
-# pacMan Game
-import tkinter as tk
 import random
-# create main window
+import tkinter as tk
+# variables to set the game up.
+# sets up a window
 window = tk.Tk()
+# create a window title
+window.title ("Hangman")
 
-# create name of title
-window.title ("Pac Man")
+words = ["chicken", "linguine", "soup", "greenbeans", "mashed potatoes"]
+word = random.choice(words)
+wordLength = len(word)
+line = "_"* wordLength
+guesses = []
+max_guesses = 5
+display = "_" * wordLength
+# adding window title
+wordlabel = tk.Label(window, text="welcome to hangman", font=("times new roman", 14, "bold",))
+wordlabel.pack()
+# intro for label
+introLabel = tk.Label (window, text="how to play(click on the console window \n and it will guess between 1 to 5 words. \n I recommend that you go back to think of you think what the word should be") 
+introLabel.pack()
+word_Line = tk.Label(window, text= line)
 
-# setup variables
-width = 400
-height = 400
-cellsize = 30
-squareSize = 50
-moveDistance = 5
- 
-# classes and functions
-class pacManGame:
-   def __init__(self, window):
-     self.window = window
-     # think of tkinter as your friend to call methods
-     self.canvas = tk.Canvas(window, width=width, height=height, bg = "red")
-     self.canvas.pack()
-     # Creating the moving square object
-     self.pacman = self.canvas.create_arc (0, 0, squareSize, squareSize, start = 45, extent = 270, fill = "yellow")
-  
-  # create the controls to move the square using your keyboard arrow keys
-     self.window.bind("<Up>", lambda event: self.move_pacman(0, -moveDistance))
-     self.window.bind("<Down>", lambda event: self.move_pacman(0, moveDistance))
-     self.window.bind("<Left>", lambda event: self.move_pacman(-moveDistance, 0))
-     self.window.bind("<Right>", lambda event: self.move_pacman(moveDistance, 0))
-   # create pallet window
-   def createfood(self):
-    self.food = []
-    for i in range(30, width - cellsize, cellsize):
-     for y in range(30, height - cellsize, cellsize):
-      if (i, y) not in self.walls:
-        self.food.append(self.canvas.create_oval(i + 8, y + 8, i + 22, y + 22, fill="white"))
-# Check to see if Pacman reaches the game window
+# sets up a word label for each word. 
+word_Line.pack()
 
-  
-# Create pacman function
-   def move_square(self, dx, dy,):
-    self.canvas.move(self.square, dx, dy)
-   def animate_pacman(self):
-    self.canvas.itemconfig(self.pacman, start = (self.canvas.itemcget(self.pacman, "start") * 5) % 360)
-    self.root.after(100, self.animate_pacman)
-    self.animate_pacman()
-app = pacManGame(window)
+# setup input
+userInput = tk.Entry(window)
+userInput.pack()
+# print statements for start of the game.
+print("guess up to 5 words incorrectly and, you lose")
+print(wordlabel.pack)
+print("the word has", len (word), "and a letters")
+# color for the window
+guessremains = tk.Label(window, text=f"Guesses remaining: {max_guesses}", font=("Times New Roman", 12, "bold"), fg="blue")
+guessremains.pack()
+
+letter_guess = tk.Label(window, text=f"Guessed Letters: {','.join(guesses)}")
+letter_guess.pack()
+# game loops (logic)
+# functions (to handle game logic)
+def handle_Input():
+  global max_guesses, display, guesses, word_length
+  guess = userInput.get().lower()
+  userInput.delete(0, tk.END)
+  if not guess.isalpha() or len(guess) != 1:
+    return
+  if guess in word:
+      for i in range(wordLength):
+        if word[i] == guess:
+          display = display[:i] + guess + display[i + 1:]
+      word_length = len(display)
+  else:
+      max_guesses -= 1
+      guessremains.config(text=f"Guesses remaining: {max_guesses}")
+  guesses.append(guess)
+  letter_guess.config(text=f"Guessed Letters: {', '.join(guesses)}")
+  word_Line.config(text=display)
+
+# checking if the game is over
+  if max_guesses == 0 or display == word:
+      userInput.config(state=tk.DISABLED)
+      if max_guesses == 0:
+        tk.messagebox.showinfo("Hangman", "You Lose!")
+      else:
+        tk.messagebox.showinfo("Hangman", "You Win")
+userInput.bind("<Return>", lambda event: handle_Input())
+
+# Start the tkinter mainloop
 window.mainloop()
